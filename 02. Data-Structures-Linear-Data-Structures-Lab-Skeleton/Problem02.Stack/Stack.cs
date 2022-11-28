9d5fcd321e547c071d8391413365a91dd47b3e7d
Slav402 <s.zekow@gmail.com>
2022-11-28 21:14:46 +0200

    adding problem 02.Stack :)

--- 02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem02.Stack/Stack.cs
+++ 02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem02.Stack/Stack.cs
@@ -8,41 +8,86 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
         }
+
 
         private Node top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; private set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item, this.top);
+            this.top = node;
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldTop = this.top;
+            this.top = oldTop.Next;
+            this.Count--;
+
+            return oldTop.Element;
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.top.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            var current = this.top;
+
+            while (current != null)
+            {
+                if (current.Element.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.top;
+
+            while(node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
         }
     }
 }
