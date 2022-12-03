4be085b742366523bce442c6abfa66de219cc31d
Slav402 <s.zekow@gmail.com>
2022-12-03 21:46:55 +0200

    fixing some bugs

---
+++ 05.Trees-Representation-and-Traversal-(BFS-DFS)-Exercise-Skeleton/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo
+{
+    using System;
+    using Tree;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new string[] { "9 17", "9 4", "9 14", "4 36", "14 53", "14 59", "53 67", "53 73" };
+
+            var treeFactory = new IntegerTreeFactory();
+
+            var tree = treeFactory.CreateTreeFromStrings(input);
+        }
+    }
+}
