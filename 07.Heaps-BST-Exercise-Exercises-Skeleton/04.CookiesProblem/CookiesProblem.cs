64c1ee852905ea2c7b409a24c8e19bf9f57dc1fc
Slav402 <s.zekow@gmail.com>
2022-12-10 03:12:06 +0200

    add problem 04.CookiesProblem

--- 07.Heaps-BST-Exercise-Exercises-Skeleton/04.CookiesProblem/CookiesProblem.cs
+++ 07.Heaps-BST-Exercise-Exercises-Skeleton/04.CookiesProblem/CookiesProblem.cs
@@ -9,7 +9,27 @@ namespace _04.CookiesProblem
     {
         public int Solve(int minSweetness, int[] cookies)
         {
-            throw new NotImplementedException();
+            var priorityQueue = new OrderedBag<int>();
+
+            priorityQueue.AddMany(cookies);
+
+            int currentMinSwitnes = priorityQueue[0];
+            int steps = 0;
+
+            while (currentMinSwitnes < minSweetness && priorityQueue.Count > 1)
+            {
+                var firstCooky = priorityQueue.RemoveFirst();
+                var secondCooky = priorityQueue.RemoveFirst();
+
+                var newCooky = firstCooky + 2 * secondCooky;
+
+                priorityQueue.Add(newCooky);
+                currentMinSwitnes = priorityQueue.GetFirst();
+                steps++;
+
+            }
+
+            return currentMinSwitnes < minSweetness ? -1 : steps;
         }
     }
 }
