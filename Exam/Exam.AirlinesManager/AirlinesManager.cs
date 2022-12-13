521d28c436340c26bbc8f0dbd4133fcbb0a4fb45
Slav402 <s.zekow@gmail.com>
2022-12-13 23:00:35 +0200

    Initial push, not full solutions

---
+++ Exam/Exam.AirlinesManager/AirlinesManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.DeliveriesManager
+{
+    public class AirlinesManager : IAirlinesManager
+    {
+        private Dictionary<string, Airline> airlinesByID = new Dictionary<string, Airline>();
+        private Dictionary<string, Flight> flightsById = new Dictionary<string, Flight>();
+
+        public void AddAirline(Airline airline)
+        {
+            this.airlinesByID.Add(airline.Id, airline);
+        }
+
+        public void AddFlight(Airline airline, Flight flight)
+        {
+            this.flightsById.Add(flight.Id, flight);
+
+            if (!airlinesByID.ContainsKey(airline.Id))
+            {
+                throw new ArgumentException();
+            }
+
+            this.airlinesByID[airline.Id].Flights.Add(flight);
+        }
+
+        public bool Contains(Airline airline)
+        {
+            return airlinesByID.ContainsKey(airline.Id);
+        }
+
+        public bool Contains(Flight flight)
+        {
+            return flightsById.ContainsKey(flight.Id);
+        }
+
+        public void DeleteAirline(Airline airline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Airline> GetAirlinesOrderedByRatingThenByCountOfFlightsThenByName()
+        {
+            return this.GetAirlines().OrderByDescending(x => x.Rating).ThenByDescending(x => x.Flights.Count).ThenBy(x => x.Name);
+        }
+
+        public IEnumerable<Airline> GetAirlines()
+        {
+            return this.airlinesByID.Values;
+        }
+
+        public IEnumerable<Airline> GetAirlinesWithFlightsFromOriginToDestination(string origin, string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Flight> GetAllFlights()
+        {
+            return this.flightsById.Values;
+        }
+
+        public IEnumerable<Flight> GetCompletedFlights()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Flight> GetFlightsOrderedByCompletionThenByNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Flight PerformFlight(Airline airline, Flight flight)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exam/Exam.DeliveriesManager/DeliveriesManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.DeliveriesManager
+{
+    public class DeliveriesManager : IDeliveriesManager
+    {
+        private Dictionary<string, Deliverer> deliverersById = new Dictionary<string, Deliverer>();
+        private Dictionary<string, Package> packegesById = new Dictionary<string, Package>();
+        
+        public void AddDeliverer(Deliverer deliverer)
+        {
+            this.deliverersById.Add(deliverer.Id, deliverer);
+        }
+
+        public void AddPackage(Package package)
+        {
+            this.packegesById.Add(package.Id, package);
+        }
+
+        public void AssignPackage(Deliverer deliverer, Package package)
+        {
+            this.deliverersById[deliverer.Id].Packages.Add(package);
+        }
+
+        public bool Contains(Deliverer deliverer)
+        {
+            return deliverersById.ContainsKey(deliverer.Id);
+        }
+
+        public bool Contains(Package package)
+        {
+            return packegesById.ContainsKey(package.Id);
+        }
+
+        public IEnumerable<Deliverer> GetDeliverers()
+        {
+            return this.deliverersById.Values;
+        }
+
+        public IEnumerable<Deliverer> GetDeliverersOrderedByCountOfPackagesThenByName()
+        {
+            return this.GetDeliverers().OrderByDescending(x => x.Packages.Count).ThenBy(x => x.Packages);
+        }
+
+        public IEnumerable<Package> GetPackages()
+        {
+            return this.packegesById.Values;
+        }
+
+        public IEnumerable<Package> GetPackagesOrderedByWeightThenByReceiver()
+        {
+            return this.GetPackages().OrderByDescending(x => x.Weight).ThenBy(x => x.Receiver);
+        }
+
+        public IEnumerable<Package> GetUnassignedPackages()
+        {
+            return this.GetPackages().Where(x => x.de == null);
+            //throw new System.NotImplementedException();
+        }
+    }
+}
